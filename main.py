87681ac31ad5c49efa10e227e3935a98eaed3161
oscarmccabe1998 <oscarmccabe98@gmail.com>
2023-03-02 16:29:23 +0000

    removed unused function

--- main.py
+++ main.py
@@ -29,40 +29,6 @@ def control_loop():
     else:
         print("There was some error")
 
-def decodeQR():
-    filename = "qr.png"
-
-    image = cv2.imread(filename)
-
-    # initialize the cv2 QRCode detector
-    detector = cv2.QRCodeDetector()
-
-    # detect and decode
-    data, vertices_array, binary_qrcode = detector.detectAndDecode(image)
-
-    # if there is a QR code
-    # print the data
-    if vertices_array is not None:      #gets JSON data from the QR code presented
-        print("QRCode data:")
-        newData = json.loads(data)
-        email = newData['email']
-        pwd = newData['oTP']
-        print(email)
-        print(pwd)
-        response = query(email)         #calls funciton in conn.py to get info from db document
-        if response != "query is null":
-            res = verify(response.pwd, pwd)
-            if res == True:
-                insert(response)
-            else:
-                print("QR too old")     #handled cases for invalid QR codes
-                removeOutliers()
-        else:
-            print("err")
-
-    else:
-        print("There was some error")
-
 def verify(hashedpw, pwd):          #verifys bcrypt string held in database 
     print(hashedpw)
     print(pwd)
